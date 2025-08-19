55ff503d87f04f243d29ad170cc1f0960aaecc3f
Kádár Máté <kafarmate99@gmail.com>
2025-08-19 10:07:18 +0200

    feat: implement input validation with INotifyDataErrorInfo
    - Create ValidationViewModelBase class implementing INotifyDataErrorInfo interface
    - Add FirstName property validation in CustomerItemViewModel
    - Display validation errors in tooltips and inline error templates

---
+++ WiredBrainCoffee.CustomersApp/ViewModel/ValidationViewModelBase.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+
+namespace WiredBrainCoffee.CustomersApp.ViewModel
+{
+    public class ValidationViewModelBase : ViewModelBase, INotifyDataErrorInfo
+    {
+        private readonly Dictionary<string, List<string>> _errorsByPropertyName = new();
+
+        public bool HasErrors => _errorsByPropertyName.Any();
+
+        public event EventHandler<DataErrorsChangedEventArgs>? ErrorsChanged;
+
+        public IEnumerable GetErrors(string? propertyName)
+        {
+            return propertyName is not null && _errorsByPropertyName.ContainsKey(propertyName)
+                ? _errorsByPropertyName[propertyName]
+                : Enumerable.Empty<string>();
+        }
+        protected virtual void OnErrorsChanged(DataErrorsChangedEventArgs e)
+        {
+            ErrorsChanged?.Invoke(this, e);
+        }
+
+        protected void AddError(string error,
+            [CallerMemberName] string? propertyName = null)
+        {
+            if (propertyName is null) return;
+
+            // If this property has no errors tracked yet, create a new list for it
+            if (!_errorsByPropertyName.ContainsKey(propertyName))
+            {
+                _errorsByPropertyName[propertyName] = new List<string>();
+            }
+
+            // Only add the error if it’s not already in the list
+            if (!_errorsByPropertyName[propertyName].Contains(error))
+            {
+                _errorsByPropertyName[propertyName].Add(error);
+                // Notify the UI that this property's errors have changed
+                OnErrorsChanged(new DataErrorsChangedEventArgs(propertyName));
+                // Notify that the HasErrors property have changed
+                RaisePropertyChanged(nameof(HasErrors));
+            }
+        }
+
+        protected void ClearErrors([CallerMemberName] string? propertyName = null)
+        {
+            if (propertyName is null) return;
+
+            if (_errorsByPropertyName.ContainsKey(propertyName))
+            {
+                _errorsByPropertyName.Remove(propertyName);
+                OnErrorsChanged(new DataErrorsChangedEventArgs(propertyName));
+                RaisePropertyChanged(nameof(HasErrors));
+            }
+        }
+    }
+}
